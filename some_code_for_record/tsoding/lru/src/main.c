f5687373efb2e6878d797f4aba7391d0f472105d
shiyubabab <745371486@qq.com>
2026-04-23 09:19:59 +0800

    26 04 23

---
+++ some_code_for_record/tsoding/lru/src/main.c
@@ -0,0 +1,108 @@
+/*************************************************************************
+	> File Name: main.c
+	> Author: mlxh
+	> Mail: mlxh_gto@163.com 
+	> Created Time: Wed 22 Apr 2026 09:56:14 AM CST
+ ************************************************************************/
+
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
+
+#define SV_IMPLEMENTATION
+#include "sv.h"
+
+#define handle_error(msg) \
+	do { perror(msg); exit(EXIT_FAILURE); } while(0)
+
+char *path = "../shakespeare.txt";
+
+typedef struct {
+	char data[32];
+} Word;
+
+Word sv_as_word(String_View sv)
+{
+	Word result = {0};
+	assert(sv.count < sizeof(result.data));
+	memcpy(result.data,sv.data,sv.count);
+	return result;
+}
+
+Word word_norm(Word word)
+{
+	Word result = {0};
+	char *in  = word.data;
+	char *out = result.data;
+	while(*in){
+		char c = *in++;
+		if(isalnum(c)){
+			*out++ = toupper(c);
+		}
+	}
+	return result;
+}
+
+size_t word_count(String_View content, Word needle)
+{
+	size_t count = 0;
+	while(content.count > 0){
+		String_View sv_line = sv_chop_by_delim(&content,'\n');
+		while(sv_line.count > 0){
+			String_View sv_word = sv_chop_by_delim(&sv_line,' ');
+			if(sv_word.count > 0){
+				Word word = word_norm(sv_as_word(sv_word));
+				if(strcmp(word.data,needle.data) == 0){
+					count++;
+				}
+			}
+		}
+	}
+	return count;
+}
+
+int main(void)
+{
+	int fd;
+	fd = open(path,O_RDONLY);
+	if(fd == -1)
+		handle_error("open");
+
+	struct stat sb;
+	if(fstat(fd,&sb) == -1)
+		handle_error("fstat");
+
+	size_t content_size = sb.st_size;
+
+	char *content_data = mmap(NULL,content_size,PROT_READ,MAP_PRIVATE,fd,0);
+	if(content_data == MAP_FAILED)
+		handle_error("mmap");
+
+	String_View content = sv_from_part(content_data,content_size);
+
+	while(content.count > 0){
+		String_View line = sv_chop_by_delim(&content,'\n');
+
+		while(line.count > 0){
+			String_View sv_word = sv_trim(sv_chop_by_delim(&line,' '));
+			if(sv_word.count > 0){
+				Word needle = word_norm(sv_as_word(sv_word));
+				//printf("(%s)\n",word.data);
+				size_t frep = 0;
+				frep = word_count(sv_from_part(content_data,content_size),needle);
+				printf("%s(%zu) ",needle.data,frep);
+			}
+		}
+		printf("\n");
+	}
+
+	munmap(content_data,content_size);
+	close(fd);
+
+	return 0;
+}
+
